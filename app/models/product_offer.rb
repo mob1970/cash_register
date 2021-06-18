374847acb14a51562db3ea09cf7b9e5bdad5cf3b
Miquel Oliete Baliarda <miqueloliete@gmail.com>
2021-06-18 17:55:31 +0200

    Redefine validation of new price for offers

--- app/models/product_offer.rb
+++ app/models/product_offer.rb
@@ -2,5 +2,5 @@ class ProductOffer < ApplicationRecord
   belongs_to :product
 
   validates :minimum_quantity, null: false, numericality: { greater_than: 0 }
-  validates :new_price, null: false, numericality: { greater_than: 0 }
+  validates :new_price, null: false, numericality: { greater_than_or_equal_to: 0 }
 end
